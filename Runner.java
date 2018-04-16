a669369a29cbc6adfef772a4f5335d128d9f8b66
Stascha <stasarasic@gmail.com>
2018-04-16 12:33:17 +0200

    Create Runner.java

---
+++ Runner.java
@@ -0,0 +1,20 @@
+/** Created by stasa on 1.2.2018  */
+
+import java.util.*;
+
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        List<Character> a = new ArrayList<Character>();
+
+        // ovo je test primer:
+        String s = "ababcbacadefegdehijhklij";
+
+        for (int i=0; i<s.length(); i++)
+            a.add(s.charAt(i));
+
+        System.out.println((new Solution()).lengthEachScene(a));
+        System.out.println((new Solution2()).lengthEachScene2(a));
+    }
+}
